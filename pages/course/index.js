b757749a0774a48010c42c17bf08e44e947cdf6e
Ye Joo Park <subwaymatch@gmail.com>
2020-07-13 03:54:03 -0500

    Display courses list

--- pages/course/index.js
+++ pages/course/index.js
@@ -1,9 +1,27 @@
 import Layout from "components/layout";
+import { getAllCourses } from "lib/courses";
 
-export default function CourseHome() {
+export default function CourseHome({ courses }) {
   return (
     <Layout>
       <h2>Courses</h2>
+
+      {courses.map((course) => (
+        <div key={course.id}>
+          <h3>{course.title}</h3>
+          <p>{course.description}</p>
+        </div>
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const courses = await getAllCourses();
+
+  return {
+    props: {
+      courses,
+    },
+  };
 }
