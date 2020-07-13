2ac3ea2928e61a10510b91ed4d84a02579daa4d3
Ye Joo Park <subwaymatch@gmail.com>
2020-07-13 10:50:09 -0500

    Display quiz questions list

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,20 +1,24 @@
 import Link from "next/link";
 import Layout from "components/layout";
 import { getAllMultipleChoiceQuizIds } from "lib/quizzes";
+
+import styles from "./index.module.scss";
+
 export default function QuizHome(props) {
   console.log(props);
   return (
     <Layout>
       <h2>List of Multiple Choice Quizzes</h2>
 
-      {/* {multipleChoiceQuizzes.map((q) => (
+      {props.paths.map((path) => (
         <Link
+          key={path.params.id}
           href="/quiz/multiple-choice/[id]"
-          as={`/quiz/multiple-choice/${q.id}`}
+          as={`/quiz/multiple-choice/${path.params.id}`}
         >
-          <a className="quiz-link">{q.id}</a>
+          <a className={styles["quiz-link"]}>{path.params.id}</a>
         </Link>
-      ))} */}
+      ))}
     </Layout>
   );
 }
