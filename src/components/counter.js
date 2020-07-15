7c3ee716fe0011d6ec53bd0686ed3da1c1879715
Ye Joo Park <subwaymatch@gmail.com>
2020-07-15 05:11:27 -0500

    Test counter redux component

---
+++ src/components/counter.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  increment,
+  decrement,
+  reset,
+  selectCount,
+} from 'lib/slices/counterSlice';
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const count = useSelector(selectCount);
+
+  return (
+    <>
+      <div>
+        <h2>Count: {count}</h2>
+      </div>
+      <div>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import Layout from 'src/components/layout';
+import Counter from 'src/components/counter';
 
 export default function Home() {
   return (
     <Layout>
       <div className="test">
         <h2>Home</h2>
+
+        <Counter />
       </div>
     </Layout>
   );
