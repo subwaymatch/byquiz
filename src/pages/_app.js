a5b2b8a84118d0cd9ec3bb4973d00fe18f926c4a
Ye Joo Park <subwaymatch@gmail.com>
2020-07-15 04:55:17 -0500

    Add redux store using redux-toolkit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import 'src/styles/global.scss';
+import { Provider } from 'react-redux';
+import store from 'src/store';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
