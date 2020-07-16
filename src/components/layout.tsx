854371c28618641d1adc25c4a62127d9a932c637
Ye Joo Park <subwaymatch@gmail.com>
2020-07-16 02:15:41 -0500

    Change layout component to TypeScript

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
-export default function Layout({ children }) {
+type LayoutProps = { children: React.ReactNode };
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <div className="wrapper">
       <Head>
