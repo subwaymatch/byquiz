1ac4883f5e3c32cd28509749c32d639785a63afc
Ye Joo Park <subwaymatch@gmail.com>
2020-07-17 07:35:00 -0500

    Convert remaining JS portion to TypeScript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import 'src/styles/global.scss';
+import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import store from 'src/store';
+import 'src/styles/global.scss';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
