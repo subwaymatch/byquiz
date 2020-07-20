032c167398f9d596d61ae6f195728d292a8140fa
Ye Joo Park <subwaymatch@gmail.com>
2020-07-20 08:10:37 -0500

    Log Firebase credentials for debugging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,11 @@ import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import store from 'src/store';
 import 'src/styles/global.scss';
-import UserProvider from 'src/context/userContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <UserProvider>
-        <Component {...pageProps} />
-      </UserProvider>
+      <Component {...pageProps} />
     </Provider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
 import Layout from 'src/components/layout';
 import Counter from 'src/components/counter';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
+
   return (
     <Layout>
       <div className="test">
-        <h2>Home</h2>
-
         <Counter />
       </div>
     </Layout>
   );
 }
+
+export const getStaticProps = () => {
+  return {
+    props: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY.slice(0, 5),
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    },
+  };
+};
