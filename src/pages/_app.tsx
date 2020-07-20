c659be606e286062211fddfe8b80930ef79d569f
Ye Joo Park <subwaymatch@gmail.com>
2020-07-20 08:00:53 -0500

    Add Firebase UserContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import store from 'src/store';
 import 'src/styles/global.scss';
+import UserProvider from 'src/context/userContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
     </Provider>
   );
 }
