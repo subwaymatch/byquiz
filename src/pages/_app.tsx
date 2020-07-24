c05b1f1d58344768c08d27a15daf3ca69e8dfb07
Ye Joo Park <subwaymatch@gmail.com>
2020-07-24 08:11:16 -0500

    Add todos test component to index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,30 @@
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import firebase from 'src/firebase/clientApp';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+
 import store from 'src/store';
 import 'src/styles/global.scss';
+
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true,
+};
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <Component {...pageProps} />
+      </ReactReduxFirebaseProvider>
     </Provider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Layout from 'src/components/layout';
 import Counter from 'src/components/counter';
+import TodosComponent from 'src/components/todo';
 
 export default function Home(props) {
   return (
     <Layout>
       <div className="test">
         <Counter />
+        <TodosComponent />
       </div>
     </Layout>
   );
