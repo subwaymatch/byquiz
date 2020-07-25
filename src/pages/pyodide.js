4cf4c7153b506e73f071042fe2af69bad6a54233
Ye Joo Park <subwaymatch@gmail.com>
2020-07-25 12:00:18 -0500

    Deployment build test: remove .tsx in pyodide.js import

--- src/pages/pyodide.js
+++ src/pages/pyodide.js
@@ -1,5 +1,5 @@
 import PyodideWrapper from '../components/pyodide-wrapper';
-import Layout from '../components/layout.tsx';
+import Layout from '../components/layout';
 
 export default function PyodideTestPage() {
   return (
