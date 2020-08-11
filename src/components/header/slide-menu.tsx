6ebc632b8bad99f65f275c4940187b14bdc1ad86
Ye Joo Park <subwaymatch@gmail.com>
2020-08-11 04:04:28 -0500

    Remove outline in react-burger-menu icon

--- src/components/header/slide-menu.tsx
+++ src/components/header/slide-menu.tsx
@@ -3,41 +3,39 @@ import { push as Menu } from 'react-burger-menu';
 
 export default function SlideMenu() {
   return (
-    <Menu
-      customBurgerIcon={<img src="/images/icon-burger-bars.svg" />}
-      pageWrapId="page-wrap"
-      outerContainerId="layout-wrapper"
-      right
-    >
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/course">
-        <a>Courses</a>
-      </Link>
-      <Link href="/question">
-        <a>Question Bank</a>
-      </Link>
+    <>
+      <Menu
+        customBurgerIcon={<img src="/images/icon-burger-bars.svg" />}
+        pageWrapId="page-wrap"
+        outerContainerId="layout-wrapper"
+        right
+      >
+        <Link href="/">
+          <a className="menu-item">Home</a>
+        </Link>
+        <Link href="/course">
+          <a className="menu-item">Courses</a>
+        </Link>
+        <Link href="/question">
+          <a className="menu-item">Question Bank</a>
+        </Link>
+      </Menu>
 
       <style jsx global>
         {`
           /* Position and sizing of burger button */
           .bm-burger-button {
             position: fixed;
-            width: 36px;
+            width: 40px;
             height: 30px;
-            left: 36px;
-            top: 36px;
+            top: 0rem;
+            right: 0rem;
+            background: #fc3;
+            padding: 5px;
           }
 
-          /* Color/shape of burger icon bars */
-          .bm-burger-bars {
-            background: #373a47;
-          }
-
-          /* Color/shape of burger icon bars on hover*/
-          .bm-burger-bars-hover {
-            background: #a90000;
+          .bm-burger-button button {
+            outline: none;
           }
 
           /* Position and sizing of clickable cross button */
@@ -62,9 +60,9 @@ Note: Beware of modifying this element as it can break the animations - you shou
 
           /* General sidebar styles */
           .bm-menu {
-            background: #373a47;
+            background: white;
             padding: 2.5em 1.5em 0;
-            font-size: 1.15em;
+            font-size: 1.1rem;
           }
 
           /* Morph shape necessary with bubble or elastic */
@@ -79,8 +77,8 @@ Note: Beware of modifying this element as it can break the animations - you shou
           }
 
           /* Individual item */
-          .bm-item {
-            display: inline-block;
+          .menu-item {
+            display: block;
           }
 
           /* Styling of overlay */
@@ -89,6 +87,6 @@ Note: Beware of modifying this element as it can break the animations - you shou
           }
         `}
       </style>
-    </Menu>
+    </>
   );
 }
