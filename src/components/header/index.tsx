1dc8261b3b3a1e082aff5f2543707291d635089b
Ye Joo Park <subwaymatch@gmail.com>
2020-08-15 04:50:50 -0500

    Add motion-framer for animations

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 import styles from './header.module.scss';
+import { motion } from 'framer-motion';
 import { FaPython } from 'react-icons/fa';
 import { BsFillLayersFill } from 'react-icons/bs';
-import { AiFillBank } from 'react-icons/ai';
 import { IoIosFiling } from 'react-icons/io';
 import { useRouter } from 'next/router';
 import classNames from 'classnames/bind';
@@ -30,16 +30,22 @@ export default function HeaderComponent({ headerTitle }: HeaderComponentProps) {
         ) : (
           <nav className={styles.headerMainMenu}>
             <Link href="/course">
-              <a
+              <motion.a
                 className={cx('menuItem', {
                   active:
                     router.pathname === '/course' ||
                     router.pathname.startsWith('/course/'),
                 })}
+                whileHover={{
+                  y: 2,
+                  transition: {
+                    duration: 0.1,
+                  },
+                }}
               >
                 <BsFillLayersFill className={cx('menuIcon')} />
                 <span>Courses</span>
-              </a>
+              </motion.a>
             </Link>
             <Link href="/question">
               <a
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,33 @@
 import Layout from 'src/components/layout';
 import styles from './home.module.scss';
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
     <Layout>
       <div className="row">
         <div className="col-12">
-          <p className={styles.heroText}>Learn anything by solving problems.</p>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.8,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.4,
+                },
+              },
+            }}
+          >
+            <p className={styles.heroText}>
+              Learn anything by solving problems.
+            </p>
+          </motion.div>
 
           <div className={styles.heroImageWrapper}>
             <img
